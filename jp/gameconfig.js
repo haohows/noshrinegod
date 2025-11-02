1c5226a5c51e4c693d5061bc80d2a58b7221b06b
黃阿皓 <faraway520@gmail.com>
2025-11-02 19:25:16 +0800

    更新

--- jp/gameconfig.js
+++ jp/gameconfig.js
@@ -1,1 +1,1 @@
-window.gameconfig={counter:"https://www.f-counter.net/j/66/1761960110/",feedbackUrl:"https://forms.gle/UP6vXKMymEZoagdR9",shareUrl:"https://haohows.com/noshrinegod/jp",shareText:"オレは王大熊。ガキの頃から路地裏の廟で育った、ちょっとばかし腕が立つ心霊調査員だ。名古屋から帰ってきてからというもの、ずっと胸騒ぎがしていた。気持ちを落ち着けるために龍山寺へお参りし、ついでにおみくじを引いたんだが――まさか、そのまま妙な事件に巻き込まれるとはな..."};
+window.gameconfig={feedbackUrl:"https://forms.gle/UP6vXKMymEZoagdR9",shareUrl:"https://haohows.com/noshrinegod/jp",shareText:"オレは王大熊。ガキの頃から路地裏の廟で育った、ちょっとばかし腕が立つ心霊調査員だ。名古屋から帰ってきてからというもの、ずっと胸騒ぎがしていた。気持ちを落ち着けるために龍山寺へお参りし、ついでにおみくじを引いたんだが――まさか、そのまま妙な事件に巻き込まれるとはな..."};
